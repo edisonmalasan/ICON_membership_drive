6f655eb9652aa29d161b0482bc822bb594f83955
Lestat <2233915@slu.edu.ph>
2025-08-17 23:59:09 +0800

    feat: add admin routes for member management

--- server/v2/api/routes/memberRoutes.js
+++ server/v2/api/routes/memberRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const {handleGetMembers, handleGetCount, handleExportCSV} = require('../controller/adminController.js');
+const {handleGetMembers, handleGetCount, handleExportCSV} = require('../controller/memberController.js');
 
 const adminRoutes = express.Router();
 adminRoutes.use(express.json());
 
 adminRoutes.get('/members', handleGetMembers);
+adminRoutes.post('/members', handlePostMember);
 adminRoutes.get('/count', handleGetCount);
 adminRoutes.get('/export/csv', handleExportCSV);
 

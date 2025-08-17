d18decec5370c45628ab8b7a2b5001e025b5bb34
Lestat <2233915@slu.edu.ph>
2025-08-17 23:24:22 +0800

    feat: implement MongoDB connection utility functions

---
+++ server/v2/utils/mongoUtil.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const MONGOOSE_URI = "mongodb://mongodb:27017"
+
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(MONGOOSE_URI, {
+            dbName: "membership_drive",
+        });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
+}
+async function disconnectFromDatabase() {
+    try {
+        await mongoose.disconnect();
+        console.log('Disconnected from MongoDB');
+    } catch (error) {
+        console.error('Error disconnecting from MongoDB:', error);
+        throw error;
+    }
+}
+
+// Handle MongoDB connection events
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Disconnected from MongoDB');
+});
+
+
+module.exports = {
+    connectToDatabase,
+    disconnectFromDatabase,
+};

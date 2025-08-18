bdfe10ddf4c9035e190c0fd60de91c2f91972141
Lestat <2233915@slu.edu.ph>
2025-08-18 22:45:00 +0800

    feat: add validateToken middleware to member routes for enhanced security

--- server/v2/api/routes/memberRoutes.js
+++ server/v2/api/routes/memberRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
+const {validateToken} = require('../../middleware/authMiddleware.js');
+
 const {handleGetMembers, handleGetCount, handleExportCSV, handlePostMember} = require('../controller/memberController.js');
 
 const memberRoutes = express.Router();
 memberRoutes.use(express.json());
 
-memberRoutes.get('/', handleGetMembers);
+memberRoutes.get('/', validateToken, handleGetMembers);
 memberRoutes.post('/', handlePostMember);
 memberRoutes.get('/count', handleGetCount);
-memberRoutes.get('/export/csv', handleExportCSV);
+memberRoutes.get('/export/csv', validateToken, handleExportCSV);
 
 module.exports = memberRoutes;

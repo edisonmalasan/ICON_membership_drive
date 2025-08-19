0a8125036542ee9c968a83de772d4b01386ad83a
hanselijahv <hanselijahv@gmail.com>
2025-08-19 22:11:25 +0800

    feat(login-page): finalize UI and styling for login page

--- frontend/src/components/theme-toggle.jsx
+++ frontend/src/components/theme-toggle.jsx
@@ -8,7 +8,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { useTheme } from "@/components/theme-provider"
 
-export function ModeToggle() {
+export function ThemeToggle() {
   const { setTheme } = useTheme()
 
   return (
@@ -26,9 +26,6 @@ export function ModeToggle() {
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>

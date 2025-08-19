920fc906815c2939a1681d9c37d513a9099262fa
Lestat <2233915@slu.edu.ph>
2025-08-19 22:07:00 +0800

    feat: add authRoutes to v2 router for improved routing structure

--- server/v2/v2.js
+++ server/v2/v2.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const memberRoutes = require('./api/routes/memberRoutes.js');
+const authRoutes = require('./api/routes/authRoutes.js');
 
 const v2Routes = express.Router();
 
 v2Routes.use('/members', memberRoutes);
+v2Routes.use('/auth', authRoutes);
+
 
 module.exports = v2Routes;

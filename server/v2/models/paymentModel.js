60f55ad633b252d2de0882a3553c84505bcf5ddc
Lestat <2233915@slu.edu.ph>
2025-08-23 17:27:47 +0800

    refactor: standardize payment schema formatting for improved readability

--- server/v2/models/paymentModel.js
+++ server/v2/models/paymentModel.js
@@ -1,37 +1,43 @@
 const mongoose = require('mongoose');
 
 const paymentSchema = mongoose.Schema({
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Member',
-        required: true
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Member",
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  paymentMethod: {
+    type: String,
+    enum: ["Cash", "Digital"],
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["Unpaid", "Paid", "Pending"],
+    default: function () {
+      if (this.paymentMethod === "Digital") {
+        return "Pending";
+      }else{
+        return "Unpaid";
+      }
     },
-    amount:{
-        type: Number,
-        required: true
-    },
-    status:{
-        type: String,
-        required: true,
-        enum:['Unpaid','Paid','Pending'],
-        default: 'Unpaid'
-    },
-    paymentMethod:{
-        type: String,
-        enum: ['Cash','Digital'],
-    },
-    transactionId:{
-        type:String,
-        required: true
-    },
-    remarks:{
-        type:String,
-        required: true
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    }
-})
+  },
+  transactionId: {
+    type: String,
+    required: true,
+  },
+  remarks: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model('Payment', paymentSchema);

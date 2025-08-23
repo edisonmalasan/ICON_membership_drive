8cfa247840e6125272abdc500ded0c916bb1a6f8
Lestat <2233915@slu.edu.ph>
2025-08-24 01:14:37 +0800

    feat: add updateMember functionality and corresponding route

--- server/v2/api/routes/memberRoutes.js
+++ server/v2/api/routes/memberRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const {validateToken} = require('../../middleware/authMiddleware.js');
 
-const {handleGetMembers, handleGetCount, handleExportCSV, handlePostMember} = require('../controller/memberController.js');
+const {handlePutMember, handleGetMembers, handleGetCount, handleExportCSV, handlePostMember} = require('../controller/memberController.js');
 
 const memberRoutes = express.Router();
 memberRoutes.use(express.json());
 
 memberRoutes.get('/', validateToken('admin'), handleGetMembers);
 memberRoutes.post('/', validateToken('guest','admin','superuser'), handlePostMember);
+memberRoutes.put('/:id', validateToken('admin','superuser'), handlePutMember)
 memberRoutes.get('/count', validateToken('guest'), handleGetCount);
 memberRoutes.get('/export/csv', validateToken('admin'), handleExportCSV);
 

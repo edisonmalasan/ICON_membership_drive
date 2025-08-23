0bb3d96deb20ae7e3920c458d79a62d91d3a05c2
Lestat <2233915@slu.edu.ph>
2025-08-23 12:45:14 +0800

    fix: change year field type to Number and update enum values in member schema

--- server/v2/models/memberModel.js
+++ server/v2/models/memberModel.js
@@ -8,9 +8,9 @@ const memberSchema = new mongoose.Schema({
     trim: true
   },
   year: {
-    type: String,
+    type: Number,
     required: true,
-    enum: ['1st Year', '2nd Year', '3rd Year', '4th Year', 'Graduate']
+    enum: [1, 2, 3, 4, 5]
   },
   course: {
     type: String,
@@ -37,7 +37,7 @@ const memberSchema = new mongoose.Schema({
   joinedAt: {
     type: Date,
     default: Date.now
-  }
+  },
 });
 
 module.exports = mongoose.model('Member', memberSchema);

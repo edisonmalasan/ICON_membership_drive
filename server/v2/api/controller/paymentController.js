b0a7540385f77bf4bc88dad360b840c50abf3e07
Lestat <2233915@slu.edu.ph>
2025-08-23 12:45:30 +0800

    feat: add payment controller with GET, POST, and PUT handlers

---
+++ server/v2/api/controller/paymentController.js
@@ -0,0 +1,18 @@
+
+async function handleGetPayment(req, res){
+
+} 
+
+async function handlePostPayment(req,res){
+
+}
+
+async function handlePutPayment(req,res){
+
+}
+
+module.exports = {
+    handleGetPayment,
+    handlePostPayment,
+    handlePutPayment
+}

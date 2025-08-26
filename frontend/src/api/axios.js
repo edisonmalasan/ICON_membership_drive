460487c7b8f7ab3b57a3b4fa38bd4c6a6657bc89
Lestat <2233915@slu.edu.ph>
2025-08-26 19:34:25 +0800

    fix: update API_URL to use localhost for development

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://apicon.lestat.cloud/api/v2",
+  baseURL: "https://localhost:3000/api/v2",
   headers: {
     "Content-Type": "application/json",
   },
